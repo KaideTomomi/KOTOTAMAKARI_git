3900b4a0b47226eabc36318181ea9cf6685d02cc
KaideTomomi <kurohamudx@gmamil.com>
2018-06-20 13:02:31 +0900

    4kaime
    シーン間をスムーズに移動できるようになった！！！！！！！！！！！！！

---
+++ Assets/script/SetPosition.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SetPosition : MonoBehaviour {
+	public GameObject playerObject ;
+	public float offset ;
+
+	// Use this for initialization
+	void Start () {
+		if ( PositionManager.hasPos( SceneManager.GetActiveScene().name ) ){
+			Vector3 pos = PositionManager.getPos( SceneManager.GetActiveScene().name );
+			playerObject.transform.position = new Vector3( pos.x+ offset, pos.y, pos.z );
+		}
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
